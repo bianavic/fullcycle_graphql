db574bdbe3dce584cec807a08c1855e8627067f5
bianavic <bianavic@gmail.com>
2025-02-15 09:58:29 -0300

    fix

--- graph/resolver.go
+++ graph/resolver.go
@@ -1,9 +1,12 @@
 package graph
 
+import "github.com/bianavic/fullcycle_graphql/internal/database"
+
 // This file will not be regenerated automatically.
 //
 // It serves as dependency injection for your app, add any dependencies you require here.
 
 type Resolver struct {
-	// Todos []*model.Todo
+	CategoryDB *database.Category
+	CourseDB   *database.Course
 }
